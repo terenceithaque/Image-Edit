503d0f22e868aeaf18cdf4919b0ee813a1c975e9
terenceithaque <terence.lethierry@protonmail.com>
2023-03-12 12:02:55 +0100

    declared showImage as external function and added 'label' and 'file' parameter

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ image_label.pack()
 
 file_menu = Menu(menu_bar, tearoff=0) # Creating "File" menu
 
-file_menu.add_command(label = "Open file  Ctrl + O", command = lambda:OpenFile(image_label, window))
-keyboard.add_hotkey("Ctrl + O", lambda : OpenFile(image_label, window))
+file_menu.add_command(label = "Open file  Ctrl + O", command = lambda:OpenFile(image_label,window))
+keyboard.add_hotkey("Ctrl + O", lambda : OpenFile(image_label,window))
 
 file_menu.add_command(label = "Quit app Ctrl + Q", command = lambda:quit(window))
 keyboard.add_hotkey("Ctrl + Q", lambda:quit(window))
 
 
-menu_bar.add_cascade(label = "Fichier", menu = file_menu) # Integrate "File" menu to menu bar
+menu_bar.add_cascade(label = "File", menu = file_menu) # Integrate "File" menu to menu bar
 window.config(menu = menu_bar)
 
 window.mainloop() # Start window main loop
--- openfile.py
+++ openfile.py
@@ -4,25 +4,23 @@ from tkinter import filedialog, messagebox, Label # Import file dialog and  mess
 from PIL import ImageTk # Importer le module ImageTk de PIL
 
 
+def showImage(label, file):
+        "Show opened image"
+        global image
+
+        image = ImageTk.PhotoImage(file = file)
+
+        label.config(image = image)
+        label.image = image    
+        
 
 def OpenFile(label, master_window):
     "Ask for a file to open"
     try:
         file = filedialog.askopenfilename(initialdir= "C:", filetypes= [("Fichiers image", (".jpg", ".png"))]) # Ask user what file to open
         master_window.title("{} - Image-Edit".format(file))
-       
-
-        print(file)
-        def showImage():
-            image = ImageTk.PhotoImage(file = file)
-            label.config(image = image)
-            label.image = image
-            
-            
-
-
-
-        showImage()
+    
+        showImage(label = label,file = file)
 
     except FileNotFoundError:
         messagebox.showerror("Fichier introuvable", "le fichier {} est introuvable".format(file))
